c089baf9530c8d26ce22a4515d165cf6b75759d5
Pixelshot <pixelshot8@gmail.com>
2021-10-26 09:56:19 +0800

    First commit. 16:53. Focusing on making j.d transition looking smoother

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,24 @@
+import "./intro.css";
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is</h2>
+          <h1 className="i-name">Hazri</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Director of Operations</div>
+              <div className="i-title-item">Web Developer</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="i-right">right</div>
+    </div>
+  );
+};
+
+export default Intro;
